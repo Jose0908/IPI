2cb7f1f6569b658500f614e1ebdd91a5ba109acb
Jose Soares <joserisoares2001@gmail.com>
2024-09-13 21:34:05 -0300

    codigo novo

--- Codigo/codigo.py
+++ Codigo/codigo.py
@@ -3,6 +3,33 @@ import numpy as np
 import matplotlib.pyplot as plt
 from collections import deque
 
+def brightGray(image):
+    # Converter a imagem para float32 para operações aritméticas precisas
+    image = image.astype(np.float32)
+    
+    # Separar os canais de cor (B, G, R)
+    B = image[:, :, 0]
+    G = image[:, :, 1]
+    R = image[:, :, 2]
+    
+    # Criar máscaras baseadas nas condições
+    mask_brilho = (np.abs(R - G) < 25) & (np.abs(R - B) < 25) & (np.abs(G - B) < 25)
+    mask_cor = (R > 110) & (R < 200) & (G > 110) & (G < 200) & (B > 110) & (B < 200)
+    
+    # Combinar as máscaras
+    mask = mask_brilho & mask_cor
+    
+    # Aplicar as operações usando as máscaras
+    image[mask] += 50
+    image[~mask] /= 2
+    
+    # Garantir que os valores estejam dentro do intervalo [0, 255]
+    image = np.clip(image, 0, 255)
+    
+    # Converter a imagem de volta para uint8
+    image = image.astype(np.uint8)
+    
+    return image
 
 def convert_to_gray(img):
     # Função para converter a imagem RGB para escala de cinza usando a fórmula dada
@@ -35,19 +62,6 @@ def plot_images(images_titles):
 
     plt.tight_layout()
     plt.show()
-
-def look_around(near_pixels):
-    # near_pixels é uma lista, se tiver algum pixel branco, retorna True
-    for pixel in near_pixels:
-        if pixel == 255:
-            return 255
-    return 0
-
-def and_pixels(pixel1, pixel2):
-    # Função para fazer a operação AND entre dois pixels
-    if pixel1 == 255 and pixel2 == 255:
-        return 255
-    return 0
 
 def connect_to_border(image):
     kernel = np.ones((2, 2), np.uint8)
@@ -101,9 +115,6 @@ def connect_to_border(image):
     return mask
 
 def remove_small_components(image, min_area):
-    """
-    Remove componentes conectados que têm área menor que `min_area`.
-    """
     # Detectar componentes conectados
     num_labels, labels, stats, _ = cv.connectedComponentsWithStats(image, connectivity=8)
 
@@ -117,77 +128,143 @@ def remove_small_components(image, min_area):
 
     return cleaned_image
 
+def get_biggest_component(image):
+    num_labels, labels, stats, _ = cv.connectedComponentsWithStats(image, connectivity=8)
+    max_area = 0
+    max_label = 0
+    # Looping through all the components
+    for i in range(1, num_labels):  # Starting from 1 to skip the background label (0)
+        area = stats[i, cv.CC_STAT_AREA]
+        if area > max_area:
+            max_area = area
+            max_label = i
+    # Returning a binary mask where the biggest component is marked as True
+    biggest_component = (labels == max_label).astype(np.uint8)
+    return biggest_component * 255  # Convert to binary image (0 or 255)
+
+def closeSmallHoles(image, min_area):
+    # Encontra os componentes conectados na imagem binária
+    num_labels, labels, stats, _ = cv.connectedComponentsWithStats(~image, connectivity=8)  # Invertendo a imagem (buracos são componentes conectados)
+    
+    # Cria uma máscara para os buracos pequenos a serem fechados
+    small_holes_mask = np.zeros_like(image, dtype=np.uint8)
+    
+    # Loop para cada componente conectado (buraco)
+    for i in range(1, num_labels):  # Começa em 1 para pular o background (0)
+        area = stats[i, cv.CC_STAT_AREA]
+        # Se a área do componente conectado for menor que min_area, fechamos o buraco
+        if area < min_area:
+            # Adiciona o componente à máscara dos buracos pequenos
+            small_holes_mask[labels == i] = 255
+    
+    # Adiciona a máscara de buracos pequenos à imagem original para fechar os buracos
+    closed_image = cv.bitwise_or(image, small_holes_mask)
+    
+    return closed_image
+
+def morphological_gradient(image):
+    kernel = np.ones((2, 2), np.uint8)
+    dilation = cv.dilate(image, kernel, iterations=1)
+    erosion = cv.erode(image, kernel, iterations=1)
+    return dilation - erosion
+
+def contour_image(gray_image, contour):
+    return cv.bitwise_or(gray_image, contour)
+
+def skeletonize(image):
+    # Elemento estruturante
+    kernel = cv.getStructuringElement(cv.MORPH_CROSS, (3, 3))
+
+    # Inicializa o flag de mudança
+    has_changed = True
+
+    # Loop até não haver mais mudanças
+    while has_changed:
+        # Aplica a dilatação
+        dilation = cv.dilate(image, kernel, iterations=1)
+
+        # Aplica a erosão
+        erosion = cv.erode(image, kernel, iterations=1)
+
+        # Subtrai a erosão da dilatação
+        temp = cv.subtract(image, erosion)
+
+        # Atualiza o flag de mudança
+        has_changed = cv.countNonZero(temp) > 0
+
+        # Atualiza a imagem
+        image = cv.bitwise_and(image, temp)
+
+    return image
+
+# Função para calcular a razão área/perímetro
+def calculate_ratio(contour):
+    area = cv.contourArea(contour)
+    perimeter = cv.arcLength(contour, True)
+    if perimeter == 0:
+        return 0
+    return np.sqrt(area) / perimeter
+
 def main():
-    # Carregar imagem
-    img_path = '../Imagens/imagem-original.png'
+    img = cv.imread('../Imagens/imagem-teste3.png', cv.IMREAD_COLOR)
+    img = brightGray(img)
 
-    # Carregar imagem em RGB (OpenCV lê em BGR por padrão)
-    img = cv.imread(img_path, cv.IMREAD_COLOR)
-
-    # Carregar threshold da imagem do artigo
-    image_threshold_print = cv.imread('../Imagens/threshold-print.png', cv.IMREAD_COLOR)
-
-    # Carregar a imagem print do artigo
-    image_removed_noise_print = cv.imread('../Imagens/removed-noise-print2.png', cv.IMREAD_COLOR)
+    #image_threshold_print = cv.imread('../Imagens/threshold-print.png', cv.IMREAD_COLOR)
+    #image_removed_noise_print = cv.imread('../Imagens/removed-noise-print2.png', cv.IMREAD_COLOR)
 
     # Converter imagem para escala de cinza
     gray_nossa = convert_to_gray(img)
-    gray_artigo = convert_to_gray(image_threshold_print)
-    gray_artigo2 = convert_to_gray(image_removed_noise_print)
 
     # Aplicar limiarização de Otsu
     _, thresholded_nossa = cv.threshold(gray_nossa, 0, 255, cv.THRESH_BINARY + cv.THRESH_OTSU)
-    _, thresholded_print = cv.threshold(gray_artigo, 0, 255, cv.THRESH_BINARY + cv.THRESH_OTSU)
-    _, removed_noise_print = cv.threshold(gray_artigo2, 0, 255, cv.THRESH_BINARY + cv.THRESH_OTSU)
 
     # Remover pequenos componentes conectados à borda (ajustar `min_area` conforme necessário)
-    min_area = 100  # Ajuste o valor conforme necessário
-    cleaned_mask = remove_small_components(thresholded_print, min_area)
+    min_area = 80  
     cleaned_mask2 = remove_small_components(thresholded_nossa, min_area)
 
     # Conectar a borda
-    road_mask = connect_to_border(cleaned_mask)
     road_mask2 = connect_to_border(cleaned_mask2)
 
-    # Mostrar a imagem original e a máscara resultante
-    plt.figure(figsize=(10, 5))
-    plt.subplot(1, 2, 1)
-    plt.title('Imagem Original')
-    plt.imshow(thresholded_print, cmap='gray')
-
-    plt.subplot(1, 2, 2)
-    plt.title('Imagem Limpa (Sem Pequenos Componentes)')
-    plt.imshow(cleaned_mask, cmap='gray')
-
-    plt.show()
-
-    # Mostrar a imagem original e a máscara resultante
-    plt.figure(figsize=(10, 5))
-    plt.subplot(1, 2, 1)
-    plt.title('Imagem Original Nossa')
-    plt.imshow(thresholded_nossa, cmap='gray')
-
-    plt.subplot(1, 2, 2)
-    plt.title('Imagem Limpa (Sem Pequenos Componentes) Nossa')
-    plt.imshow(cleaned_mask2, cmap='gray')
-
-    plt.show()
-
-    # Operação de expansão com vizinhança de oito pixels (dilatação com kernel 3x3)
     kernel = np.ones((3, 3), np.uint8)
-    dilated_image = cv.dilate(road_mask, kernel, iterations=1)
+    dilated_image_nossa = cv.dilate(road_mask2, kernel, iterations=1)
 
-    # Mostrar as imagens
-    images = {
-        #'Imagem Tons de Cinza Nossa': gray_nossa,
-        #'Threshold Imagem Nossa': thresholded_nossa,
-        'Threshold Artigo Print': thresholded_print,
-        #'Imagem Sem Ruído Nossa': road_mask_nossa,
-        'Imagem Sem Ruído Artigo Print:': removed_noise_print,
-        'Imagem Sem Ruído Artigo': road_mask,
-        #'Imagem Sem Ruído Artigo Depois da Dilatação': road_mask2
-    }
-    plot_images(images)
+    # Fechar buracos pequenos
+    min_area = 570  
+    dilated_image_nossa2 = closeSmallHoles(dilated_image_nossa, min_area)
+
+    # Aplicar uma dilatacao
+    kernel = np.ones((5, 5), np.uint8)
+    opening_nossa = cv.morphologyEx(dilated_image_nossa2, cv.MORPH_CLOSE, kernel)
+
+    # Encontrar os contornos das regiões brancas
+    contours_nossa, _ = cv.findContours(opening_nossa, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+
+    # Filtrar os contornos que podem representar rodovias
+    min_ratio = 0.1  # Ajuste esse valor conforme necessário
+    
+    #mostrar cada um dos ratios
+    ratios = [calculate_ratio(cnt) for cnt in contours_nossa]
+    print(ratios)
+
+    road_contours_nossa = [cnt for cnt in contours_nossa if calculate_ratio(cnt) < min_ratio]
+
+    print (f'Número de rodovias detectadas: {len(road_contours_nossa)}')
+
+    # Criar uma imagem de saída
+    output2 = np.zeros_like(dilated_image_nossa2)
+    cv.drawContours(output2, road_contours_nossa, -1, 255, thickness=cv.FILLED)
+
+
+    # gradient = morphological_gradient(output)
+    # gradient2 = morphological_gradient(output2)
+
+    # original_contour_image = contour_image(gray_nossa, gradient2)
+
+   #Exibir o resultado
+    plt.figure(figsize=(10, 10))
+    plt.imshow(output2, cmap='gray')
+    plt.title('Rodovias Extraídas')
+    plt.show()
 
 # Função principal
 if __name__ == '__main__':
