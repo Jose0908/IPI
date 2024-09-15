4d861505fe4dd772bb83a5300234785e32e4faca
Jose Soares <joserisoares2001@gmail.com>
2024-09-15 17:16:15 -0300

    quase acabando

--- Codigo/codigo.py
+++ Codigo/codigo.py
@@ -4,94 +4,8 @@ import matplotlib.pyplot as plt
 from collections import deque
 from skimage.morphology import skeletonize
 
-
-def inverter_canal(canal):
-    # Função para inverter o canal de cor
-    return abs(255 - canal)
-
-def getGrayPixels(image):
-    #convert to hsv
-    hsv = cv.cvtColor(image, cv.COLOR_BGR2HSV)
-    
-    s_invertido = inverter_canal(hsv[:, :, 1])
-
-    #mostrar hsv
-    fig, axs = plt.subplots(1, 3, figsize=(15, 5))
-    fig.patch.set_facecolor('#323232')  # Cor de fundo da figura (cinza escuro)
-    axs[0].imshow(hsv[:, :, 0], cmap='gray', vmin=0, vmax=255)
-    axs[0].set_title('H')
-    axs[1].imshow(hsv[:, :, 1], cmap='gray', vmin=0, vmax=255)
-    axs[1].set_title('S')
-    axs[2].imshow(hsv[:, :, 2], cmap='gray', vmin=0, vmax=255)
-    axs[2].set_title('V')
-    plt.show()
-
-    #utilizar OTSU no canal H
-    _, th = cv.threshold(hsv[:, :, 0], 0, 255, cv.THRESH_BINARY + cv.THRESH_OTSU)
-
-    #utilizar OTSU no canal S invertido
-    S_invertido = inverter_canal(hsv[:, :, 1])
-    threshold = 210
-    _, th2 = cv.threshold(S_invertido, threshold, 255, cv.THRESH_BINARY)
-
-    # fazer and entre os dois resultados
-    thf = cv.bitwise_and(th, th2)
-
-    return thf
-
-    # plt.subplot(1, 3, 1)
-    # plt.imshow(th, cmap='gray', vmin=0, vmax=255)
-    # plt.title('H binarizado')
-    # plt.axis('off')  # Desativa os eixos
-
-    # Segundo subplot para 'S binarizado'
-    # plt.subplot(1, 3, 2)
-    # plt.imshow(th2, cmap='gray', vmin=0, vmax=255)
-    # plt.title('S binarizado')
-    # plt.axis('off')
-
-    # Terceiro subplot para 'And H e S binarizado'
-    # plt.subplot(1, 3, 3)
-    # plt.imshow(thf, cmap='gray', vmin=0, vmax=255)
-    # plt.title('And H e S binarizado')
-    # plt.axis('off')
-    # plt.show()
-
-def brightGray(image):
-    # Converter a imagem para float32 para operações aritméticas precisas
-    image = image.astype(np.float32)
-    
-    # Separar os canais de cor (B, G, R)
-    B = image[:, :, 0]
-    G = image[:, :, 1]
-    R = image[:, :, 2]
-    
-    # Criar máscaras baseadas nas condições
-    mask_brilho = (np.abs(R - G) < 25) & (np.abs(R - B) < 25) & (np.abs(G - B) < 25)
-    mask_cor = (R > 110) & (R < 200) & (G > 110) & (G < 200) & (B > 110) & (B < 200)
-    
-    # Combinar as máscaras
-    mask = mask_brilho & mask_cor
-    
-    # Aplicar as operações usando as máscaras
-    image[mask] += 50
-    image[~mask] /= 2
-    
-    # Garantir que os valores estejam dentro do intervalo [0, 255]
-    image = np.clip(image, 0, 255)
-    
-    # Converter a imagem de volta para uint8
-    image = image.astype(np.uint8)
-    
-    return image
-
-def convert_to_gray(img):
-    # Função para converter a imagem RGB para escala de cinza usando a fórmula dada
-    gray = np.zeros((img.shape[0], img.shape[1]), np.uint8)
-    for i in range(img.shape[0]):
-        for j in range(img.shape[1]):
-            gray[i, j] = 0.229 * img[i, j, 2] + 0.587 * img[i, j, 1] + 0.114 * img[i, j, 0]
-    return gray
+DEBUG_MODE = True
+SMALL_COMPONENT_MIN_AREA = 80
 
 def plot_images(images_titles):
     num_images = len(images_titles)
@@ -117,9 +31,47 @@ def plot_images(images_titles):
     plt.tight_layout()
     plt.show()
 
+def invert_channel(channel):
+    return abs(255 - channel)
+
+def binarize_image(image):
+    # Converter para HSV
+    hsv = cv.cvtColor(image, cv.COLOR_BGR2HSV)
+    # Inverter o canal S para melhorar a binarização
+    inverted_s = invert_channel(hsv[:, :, 1])
+
+    if (DEBUG_MODE):
+        # Mostrar os canais H, S e V
+        fig, axs = plt.subplots(1, 3, figsize=(15, 5))
+        fig.patch.set_facecolor('#323232')  # Cor de fundo da figura (cinza escuro)
+        titles = ['H', 'S', 'V']
+        for i in range(3):
+            axs[i].imshow(hsv[:, :, i], cmap='gray', vmin=0, vmax=255)
+            axs[i].set_title(titles[i])
+        plt.show()
+
+    # Binarizar o canal H utlizando OTSU
+    _, binarized_H = cv.threshold(hsv[:, :, 0], 0, 255, cv.THRESH_BINARY + cv.THRESH_OTSU)
+
+    # Binarizar o canal S utilizando threshold
+    threshold = 210
+    _, binarized_S = cv.threshold(inverted_s, threshold, 255, cv.THRESH_BINARY)
+
+    # Retornar o and entre os dois resultados
+    return cv.bitwise_and(binarized_H, binarized_S)
+
+def convert_to_gray(img):
+    # Função para converter a imagem RGB para escala de cinza usando a fórmula dada
+    gray = np.zeros((img.shape[0], img.shape[1]), np.uint8)
+    for i in range(img.shape[0]):
+        for j in range(img.shape[1]):
+            gray[i, j] = 0.229 * img[i, j, 2] + 0.587 * img[i, j, 1] + 0.114 * img[i, j, 0]
+    return gray
+
+# Extrai apenas os elementos conectados com a borda
 def connect_to_border(image):
-    kernel = np.ones((2, 2), np.uint8)
     # Fazer dilatação na imagem
+    kernel = np.ones((2, 2), np.uint8)
     image = cv.dilate(image, kernel, iterations=1)
 
     # Criar uma máscara com zeros
@@ -165,9 +117,9 @@ def connect_to_border(image):
                 if image[nx, ny] == 255 and mask[nx, ny] == 0:
                     queue.append((nx, ny))  # Adicionar o vizinho à fila
                     mask[nx, ny] = 255      # Marcar o vizinho na máscara como conectado
-
     return mask
 
+# Remove ruídos (elementos conexos muito pequenos)
 def remove_small_components(image, min_area):
     # Detectar componentes conectados
     num_labels, labels, stats, _ = cv.connectedComponentsWithStats(image, connectivity=8)
@@ -182,21 +134,10 @@ def remove_small_components(image, min_area):
 
     return cleaned_image
 
-def get_biggest_component(image):
-    num_labels, labels, stats, _ = cv.connectedComponentsWithStats(image, connectivity=8)
-    max_area = 0
-    max_label = 0
-    # Looping through all the components
-    for i in range(1, num_labels):  # Starting from 1 to skip the background label (0)
-        area = stats[i, cv.CC_STAT_AREA]
-        if area > max_area:
-            max_area = area
-            max_label = i
-    # Returning a binary mask where the biggest component is marked as True
-    biggest_component = (labels == max_label).astype(np.uint8)
-    return biggest_component * 255  # Convert to binary image (0 or 255)
+def close_small_holes(image):
+    image_size = np.sqrt(image.shape[0] * image.shape[1])
+    min_area = image_size // 2     #min_area = 170
 
-def closeSmallHoles(image, min_area):
     # Encontra os componentes conectados na imagem binária
     num_labels, labels, stats, _ = cv.connectedComponentsWithStats(~image, connectivity=8)  # Invertendo a imagem (buracos são componentes conectados)
     
@@ -218,12 +159,8 @@ def closeSmallHoles(image, min_area):
 
 def morphological_gradient(image):
     kernel = np.ones((2, 2), np.uint8)
-    dilation = cv.dilate(image, kernel, iterations=1)
-    erosion = cv.erode(image, kernel, iterations=1)
-    return dilation - erosion
-
-def contour_image(gray_image, contour):
-    return cv.bitwise_or(gray_image, contour)
+    gradient = cv.morphologyEx(image, cv.MORPH_GRADIENT, kernel)
+    return gradient
 
 # Função para calcular a razão área/perímetro
 def calculate_ratio(contour):
@@ -233,118 +170,95 @@ def calculate_ratio(contour):
         return 0
     return np.sqrt(area) / perimeter
 
-def main():
-    img = cv.imread('../Imagens/imagem-teste10.png', cv.IMREAD_COLOR)
-    thresholded_nossa = getGrayPixels(img) 
-    # Mostrar thresholded_nossa
-    #plot_images({'Imagem Original': img, 'Imagem Thresholded': thresholded_nossa})
-
-    # Remover pequenos componentes conectados à borda (ajustar `min_area` conforme necessário)
-    min_area = 80  
-    cleaned_mask = remove_small_components(thresholded_nossa, min_area)
-    #Mostrar a imagem limpa
-    #plot_images({'Imagem Original': thresholded_nossa, 'Imagem Limpa': cleaned_mask})
-
-    # Conectar a borda
-    road_mask = connect_to_border(cleaned_mask)
-    #Mostrar a imagem conectada
-    #plot_images({'Imagem Original': cleaned_mask, 'Imagem Conectada': road_mask})
-
-    # Dilatar a imagem
-    kernel = np.ones((3, 3), np.uint8)
-    dilated_image_nossa = cv.dilate(road_mask, kernel, iterations=1)
-    #Mostrar a imagem dilatada
-    #plot_images({'Imagem Original': road_mask, 'Imagem Dilatada': dilated_image_nossa})
-
-    # Fechar buracos pequenos
-    #min area é o raiz do tamanho da imagem dividido por 2
-    tamanho_imagem = np.sqrt(img.shape[0] * img.shape[1])
-    min_area = tamanho_imagem // 2
-    #min_area = 170
-    dilated_image_nossa2 = closeSmallHoles(dilated_image_nossa, min_area)
-    #Mostrar a imagem com buracos fechados
-    #plot_images({'Imagem Original': dilated_image_nossa, 'Imagem com Buracos Fechados': dilated_image_nossa2})
-
-    # Aplicar um fechamento
+#Funcao para detectar rodovias pelas formas geometricas
+def detect_road(closed_holes_image):
+    # Aplicar um fechamento para unir regiões próximas
     kernel = np.ones((5, 5), np.uint8)
-    opening_nossa = cv.morphologyEx(dilated_image_nossa2, cv.MORPH_CLOSE, kernel)
-    #Mostrar a imagem com fechamento
-    #plot_images({'Imagem Original': dilated_image_nossa2, 'Imagem com Fechamento': opening_nossa})
+    closing_nossa = cv.morphologyEx(closed_holes_image, cv.MORPH_CLOSE, kernel)
 
     # Encontrar os contornos das regiões brancas
-    contours_nossa, _ = cv.findContours(opening_nossa, cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE)
-    #ratios = [calculate_ratio(cnt) for cnt in contours_nossa]
-    #print (ratios)
-    #Mostrar os contornos
-    #plot_images({'Imagem Original': opening_nossa, 'Contornos': cv.drawContours(np.zeros_like(opening_nossa), contours_nossa, -1, 255, thickness=2)})
+    contours_nossa, _ = cv.findContours(closing_nossa, cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE)
 
     # Filtrar os contornos que podem representar rodovias
     min_ratio = 0.1  # Ajuste esse valor conforme necessário
-    #mostrar cada um dos ratios
 
-    road_contours_nossa = [cnt for cnt in contours_nossa if calculate_ratio(cnt) < min_ratio]
-    # Mostrar road_contours_nossa 
-    #plot_images({'Imagem Original': dilated_image_nossa2, 'Road Contours': cv.drawContours(np.zeros_like(dilated_image_nossa2), road_contours_nossa, -1, 255, thickness=cv.FILLED)})
+    road_contours = [cnt for cnt in contours_nossa if calculate_ratio(cnt) < min_ratio]
 
-    #print (f'Número de rodovias detectadas: {len(road_contours_nossa)}')
+    #print (f'Número de rodovias detectadas: {len(road_contours)}')
 
     # Criar uma imagem de saída
-    output = np.zeros_like(dilated_image_nossa2)
-    cv.drawContours(output, road_contours_nossa, -1, 255, thickness=cv.FILLED)
+    output = np.zeros_like(closed_holes_image)
+    cv.drawContours(output, road_contours, -1, 255, thickness=cv.FILLED)
 
-    output = cv.subtract(dilated_image_nossa2, output)
-    output2 = cv.subtract(dilated_image_nossa2, output)
-    plot_images({'Imagem Original': dilated_image_nossa2, 'Rodovias Extraídas': output2})
+    output = cv.subtract(closed_holes_image, output)
+    output2 = cv.subtract(closed_holes_image, output)
 
-    gradient = morphological_gradient(output2)
+    return output2
 
-    #transformar imagem em escala de cinza
+def road_contour_line(original_image, roads):
+    gradient = morphological_gradient(roads)
+    gray_image = convert_to_gray(original_image)
+    contour_line_image = cv.bitwise_or(gray_image, gradient)
+    contour_line_image = cv.cvtColor(contour_line_image, cv.COLOR_GRAY2BGR)
+    return gradient, contour_line_image
 
-    # Sobrepor os contornos brancos na imagem RGB original
-    # O bitwise_or é utilizado para manter os contornos brancos sobre a imagem RGB
-    
-    #converter img para escala de cinza
-    gray_nossa = convert_to_gray(img)
-
-    final_image = cv.bitwise_or(gray_nossa, gradient)
-
-    #converter para rgb
-    final_image = cv.cvtColor(final_image, cv.COLOR_BGR2RGB)
-
-    # Mostrar a imagem final com os contornos sobrepostos
-    plt.figure(figsize=(10, 10))
-    plt.imshow(final_image)
-    plt.title('Rodovias Extraídas')
-    plt.axis('off')  # Oculta os eixos
-    plt.show()
-
+def skeletonize_image(original_image, roads):
     # Normalizar a imagem para 0 e 1 (necessário para a função skeletonize)
-    output2 = output2 // 255
-
-    # Aplicar a esqueletização
-    skeleton = skeletonize(output2)
-
+    roads = roads // 255
+    skeleton = skeletonize(roads)
     # Converter de volta para 0-255 para exibição com o OpenCV/Matplotlib
     skeleton = (skeleton * 255).astype(np.uint8)
+    # Botar o esqueleto por cima da imagem original
+    gray_image = convert_to_gray(original_image)
+    skeleton_image = cv.bitwise_or(gray_image, skeleton)
+    skeleton_image = cv.cvtColor(skeleton_image, cv.COLOR_GRAY2BGR)
+    return skeleton, skeleton_image
 
-    # Mostrar a imagem esqueletizada
-    plt.figure(figsize=(10, 10))
-    plt.imshow(skeleton, cmap='gray')
-    plt.title('Imagem Esqueletizada')
-    plt.axis('off')  # Oculta os eixos
-    plt.show()
+def main():
+    img = cv.imread('../Imagens/imagem-teste2.png', cv.IMREAD_COLOR)
+    # Binarizar a imagem
+    thresholded_image = binarize_image(img) 
 
-    # jogar a imagem skeletonizada em cima da imagem original
+    # Remover pequenos componentes conexos (ajustar constante `min_area` conforme necessário)
+    cleaned_image = remove_small_components(thresholded_image, SMALL_COMPONENT_MIN_AREA)
 
-    final_image2 = cv.bitwise_or(gray_nossa, skeleton)
-    #mostrar
-    final_image2 = cv.cvtColor(final_image2, cv.COLOR_BGR2RGB)
-    plt.figure(figsize=(10, 10))
-    plt.imshow(final_image2)
-    plt.title('Rodovias Extraídas')
-    plt.axis('off')  # Oculta os eixos
-    plt.show()
+    # Pegar apenas elementos conexos a borda
+    road_with_noise = connect_to_border(cleaned_image)
 
+    # Dilatar a imagem para preencher buracos
+    kernel = np.ones((3, 3), np.uint8)
+    dilated_image = cv.dilate(road_with_noise, kernel, iterations=1)
+
+    # Fechar buracos pequenos
+    closed_holes_image = close_small_holes(dilated_image)
+
+    # Detectar rodovias pelas formas geométricas
+    roads = detect_road(closed_holes_image)
+
+    # Gerando imagem com as bordas
+    gradient, contour_line_image = road_contour_line(img, roads)
+    
+    # Gerando imagem esqueletizada
+    skeleton, skeleton_image = skeletonize_image(img, roads)
+    
+    if not DEBUG_MODE:
+        plot_images({'Imagem Original': img, 'Imagem Binarizada': thresholded_image})
+        plot_images({'Imagem Contornada': contour_line_image, 'Imagem Esqueleto': skeleton_image})
+    else:
+        # Crie uma cópia das variáveis locais que são imagens antes de iterar
+        local_vars = dict(locals())  # Faz uma cópia das variáveis locais
+        debug_variables = {key: local_vars[key] for key in local_vars if key != 'kernel' and isinstance(local_vars[key], np.ndarray)}
+        
+        debug_items = list(debug_variables.items())  # Convertemos o dicionário em uma lista de pares (nome, variável)
+        
+        for i in range(len(debug_items) - 1):
+            # Criando um dicionário com o par atual e o próximo par
+            images_to_plot = {
+                debug_items[i][0]: debug_items[i][1],     # Primeiro item do par
+                debug_items[i + 1][0]: debug_items[i + 1][1]  # Segundo item do par
+            }
+            plot_images(images_to_plot)
+        
 # Função principal
 if __name__ == '__main__':
     main()
